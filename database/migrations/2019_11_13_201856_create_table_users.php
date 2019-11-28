cebf7b641c049e6db0552bd36eec5ff20a107a52
maxivity <soroosh081@gmail.com>
2019-11-28 11:49:10 +0330

    Initial Commit

---
+++ database/migrations/2019_11_13_201856_create_table_users.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string("first_name");
+            $table->string("last_name");
+            $table->string("profile");
+            $table->string("phone_number");
+            $table->string("nationalcode");
+            $table->string("bank_account");
+            $table->integer("verified_at");
+            $table->string("email");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('table_users');
+    }
+}
